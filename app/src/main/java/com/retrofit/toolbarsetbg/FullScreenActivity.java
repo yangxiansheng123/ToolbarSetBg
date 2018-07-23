8f01db25165f3d5177501fafe3a7555fdd8191f7
yangxiansheng <1095159520@qq.com>
2018-07-23 15:21:59 +0800

    Initial commit

---
+++ app/src/main/java/com/retrofit/toolbarsetbg/FullScreenActivity.java
@@ -0,0 +1,13 @@
+package com.retrofit.toolbarsetbg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen);
+    }
+}
---
+++ app/src/main/java/com/retrofit/toolbarsetbg/MainActivity.java
@@ -0,0 +1,59 @@
+package com.retrofit.toolbarsetbg;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected Toolbar toolbarImg;
+    protected Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_main);
+        /**
+         * android4.3以上的沉浸式 ，4.3以下没效果，所以不要头部填充状态栏高度
+         */
+        int sysVersion = Build.VERSION.SDK_INT;
+        if (sysVersion > Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            int result = 0;
+            int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = getResources().getDimensionPixelSize(resourceId);
+            }
+            LinearLayout toptop = (LinearLayout) this.findViewById(R.id.toptop);
+            RelativeLayout.LayoutParams para = new RelativeLayout.LayoutParams(this.getWindowManager().getDefaultDisplay().getWidth(), result);
+            //设置修改后的布局。
+            toptop.setLayoutParams(para);
+        }
+        initView();
+    }
+
+    private void initView() {
+        toolbarImg = (Toolbar) findViewById(R.id.toolbar_img);
+        toolbarImg.setNavigationIcon(R.mipmap.left_back);
+        toolbarImg.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button) {
+            startActivity(new Intent(this, FullScreenActivity.class));
+        }
+    }
+}
